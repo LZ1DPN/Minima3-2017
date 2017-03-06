2947b5123715ce27f861da1ed52eba3d1b7d1f85
LZ1DPN <lz1dpn@yahoo.com>
2017-03-06 09:45:51 +0200

    06.03.2017
    06.03.2017

--- DDS-0-30MHz/si5351minima/si5351minima.ino
+++ DDS-0-30MHz/si5351minima/si5351minima.ino
@@ -30,10 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // new
 #include <si5351.h>
-<<<<<<< HEAD
-=======
-//#define PLLB_FREQ 87600000000ULL
->>>>>>> origin/master
 //#define SI5351_FREQ_MULT  1
 Si5351 si5351;
 //------------------------------- Set Optional Features here --------------------------------------
@@ -42,8 +38,6 @@ Si5351 si5351;
 //#define Direct_conversion //What you see on display is what you get
 //#define FreqX4  //output is four times the display frequency
 //--------------------------------------------------------------------------------------------------
-
-
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
