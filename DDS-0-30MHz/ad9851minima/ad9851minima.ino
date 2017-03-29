d3e70d58316b313ad6b566ca333ce5ac7c86016e
LZ1DPN <lz1dpn@yahoo.com>
2017-03-30 00:04:51 +0300

    30.03.2017
    30.03.2017

--- DDS-0-30MHz/ad9851minima/ad9851minima.ino
+++ DDS-0-30MHz/ad9851minima/ad9851minima.ino
@@ -1,3 +1,4 @@
+
 /*
 Revision 1.0 - Main code by Richard Visokey AD7C - www.ad7c.com
 Revision 2.0 - November 6th, 2013...  ever so slight revision by  VK8BN for AD9851 chip Feb 24 2014
@@ -9,6 +10,7 @@ Revision 6.0 - August 16, 2016  - serial control buttons from computer with USB 
 Revision 7.0 - November 30, 2016  - added some things from Ashhar Farhan's Minima TRX sketch to control transceiver, keyer, relays and other ...									
 Revision 7.5 - December 12, 2016  - for Minima and Bingo Transceivers (LZ1DPN mod).
 Revision 8.0 - February 15, 2017  - for Minima and Bingo Transceivers - IF = 20MHz(LZ1DPN mod).
+
 */
 
 #include <SPI.h>
@@ -21,35 +23,14 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
-
+  
 #if (SSD1306_LCDHEIGHT != 32)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
 // Include the library code
-//#include <LiquidCrystal.h>
 #include <rotary.h>
-#include <EEPROM.h>
 #include <avr/io.h>
 //Setup some items
 #define CW_TIMEOUT (600l) // in milliseconds, this is the parameter that determines how long the tx will hold between cw key downs
@@ -60,7 +41,6 @@ unsigned long cwTimeout = 0;     //keyer var - dead operator control
 #define CW_KEY (4)   // KEY output pin - in Q7 transistor colector (+5V when keyer down for RF signal modulation) (in Minima to enable sidetone generator on)
 #define BAND_HI (6)  // relay for RF output LPF  - (0) < 10 MHz , (1) > 10 MHz (see LPF in EK1A schematic)  
 #define USB (5)
-//#define FBUTTON (A3)  // tuning step freq CHANGE from 1Hz to 1MHz step for single rotary encoder possition
 #define ANALOG_KEYER (A1)  // KEYER input - for analog straight key
 char inTx = 0;     // trx in transmit mode temp var
 char keyDown = 0;   // keyer down temp vat
@@ -75,16 +55,16 @@ int var_start = 1;
 #define BTNDEC (A2)  // BAND CHANGE BUTTON from 1,8 to 29 MHz - 11 bands
 #define pulseHigh(pin) {digitalWrite(pin, HIGH); digitalWrite(pin, LOW); }
 Rotary r = Rotary(2,3); // sets the pins for rotary encoder uses.  Must be interrupt pins.
-//LiquidCrystal lcd(12, 13, 7, 6, 5, 4); // I used an odd pin combination because I need pin 2 and 3 for the interrupts. for LCD 16x2 - not used now
   
 int_fast32_t rx=7000000; // Starting frequency of VFO
 int_fast32_t rx2=1; // temp variable to hold the updated frequency
 int_fast32_t rxif=19996500; // IF freq, will be summed with vfo freq - rx variable
 int_fast32_t rxifLSB=19996500;  // - 3000
-int_fast32_t rxifUSB=20002500;   // + 3000
+int_fast32_t rxifUSB=20003000;   // + 3000   2500
+int_fast32_t rit=300;   //  
 
 int_fast32_t increment = 100; // starting VFO update increment in HZ. tuning step
-int buttonstate = 0;   // temp var
+int buttonstate = 0;   // temp  var
 String hertz = "100 Hz";
 int  hertzPosition = 0;
 
@@ -94,11 +74,8 @@ int_fast32_t timepassed = millis(); // int to hold the arduino miilis since star
 int memstatus = 1;  // value to notify if memory is current or old. 0=old, 1=current.
 int ForceFreq = 1;  // Change this to 0 after you upload and run a working sketch to activate the EEPROM memory.  YOU MUST PUT THIS BACK TO 0 AND UPLOAD THE SKETCH AGAIN AFTER STARTING FREQUENCY IS SET!
 int byteRead = 0;
-const int colums = 10; /// have to be 16 or 20 - in LCD 16x2 - 16, or other , see LCD spec.
-const int rows = 2;  /// have to be 2 or 4 - in LCD 16x2 - 2, or other , see LCD spec.
-int lcdindex = 0;
-int line1[colums];
-int line2[colums];
+//const int colums = 10; /// have to be 16 or 20 - in LCD 16x2 - 16, or other , see LCD spec.
+//const int rows = 2;  /// have to be 2 or 4 - in LCD 16x2 - 2, or other , see LCD spec.
 
 // buttons temp var
 int BTNdecodeON = 0;   
@@ -136,11 +113,12 @@ void checkCW(){
     //switch to transmit mode if we are not already in it
     if (inTx == 0){
         //put the  TX_RX line to transmit
-          digitalWrite(TX_RX, 0);
+         digitalWrite(TX_RX, 0);
         //give the relays a few ms to settle the T/R relays
         delay(50);
     }
     inTx = 1;
+    rit = 0;
     keyDown = 1;
     digitalWrite(CW_KEY, 1); //start the side-tone
   }
@@ -163,6 +141,7 @@ void checkCW(){
     digitalWrite(TX_RX, 1);
     digitalWrite(CW_KEY, 0);
     inTx = 0;
+    rit = 300;
     cwTimeout = 0;
   }
 }
@@ -181,9 +160,6 @@ digitalWrite(BAND_HI, LOW);
 
 pinMode(USB, OUTPUT); 
 digitalWrite(USB, LOW);
-
-//pinMode(FBUTTON, INPUT);  
-//digitalWrite(FBUTTON, 1);
   
 pinMode(TX_ON, INPUT);    // need pullup resistor see Minima schematic
 digitalWrite(TX_ON, LOW);
@@ -196,7 +172,7 @@ digitalWrite(CW_KEY, LOW);
 
 // Initialize the Serial port so that we can use it for debugging
   Serial.begin(115200);
-  Serial.println("Start VFO ver 8.0 minima 3");
+  Serial.println("Start VFO ver 10.0 minima 3");
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C address 0x3C (for oled 128x32)
@@ -220,11 +196,13 @@ digitalWrite(CW_KEY, LOW);
   digitalWrite(BTNDEC,HIGH);    // level
   pinMode(A0,INPUT); // Connect to a button that goes to GND on push - rotary encoder push button - for FREQ STEP change
   digitalWrite(A0,HIGH);  //level
-//  lcd.begin(16, 2);  // for LCD
-//  next AD9851 communication settings
+  
+//  rotary
   PCICR |= (1 << PCIE2);
   PCMSK2 |= (1 << PCINT18) | (1 << PCINT19);
   sei();
+  
+  //  next AD9851 communication settings
   pinMode(FQ_UD, OUTPUT);
   pinMode(W_CLK, OUTPUT);
   pinMode(DATA, OUTPUT);
@@ -232,8 +210,6 @@ digitalWrite(CW_KEY, LOW);
   pulseHigh(RESET);
   pulseHigh(W_CLK);
   pulseHigh(FQ_UD);  // this pulse enables serial mode on the AD9851 - see datasheet
-//  lcd.setCursor(hertzPosition,1);    
-//  lcd.print(hertz);
 
   display.clearDisplay();	
   display.setCursor(0,0);
@@ -242,16 +218,6 @@ digitalWrite(CW_KEY, LOW);
   display.println(hertz);
   display.display();
   
-   // Load the stored frequency  
-  if (ForceFreq == 0) {
-    freq = String(EEPROM.read(0))+String(EEPROM.read(1))+String(EEPROM.read(2))+String(EEPROM.read(3))+String(EEPROM.read(4))+String(EEPROM.read(5))+String(EEPROM.read(6));
-    rx = freq.toInt();  
-  }
-  
- for (int index = 0; index < colums; index++){
-    line1[index] = 32;
-	line2[index] = 32;
- }
 }
 
 ///// START LOOP - MAIN LOOP
@@ -273,17 +239,8 @@ void loop() {
 	
 // freq change 
   if (rx != rx2){
-		BTNcheck = 0;   
-		if (BTNcheck == 0) {
-			showFreq();
-            display.clearDisplay();	
-			display.setCursor(0,0);
-			display.println(rx);
-			display.setCursor(0,18);
-			display.println(hertz);
-			display.display();
-		}
-        sendFrequency(rx);
+		showFreq();
+		sendFrequency(rx);
         rx2 = rx;
       }
 
@@ -293,13 +250,6 @@ void loop() {
         setincrement();        
     };
 
-  // Write the frequency to memory if not stored and 2 seconds have passed since the last frequency change.
-    if(memstatus == 0){   
-      if(timepassed+2000 < millis()){
-        storeMEM();
-        }
-      }   
-
 // LPF band switch relay	  
 	  
 	if(rx <= 14999999){
@@ -308,56 +258,17 @@ void loop() {
 	if(rx > 14999999){
 		digitalWrite(BAND_HI, 1);
 		}
-		
+
+// BAND RELAY		
 	if(rx < 10000000){
 		rxif=rxifLSB;
-                digitalWrite(USB, 0);
+        digitalWrite(USB, 0);
 	    }
 	if(rx >= 10000000){
 		rxif=rxifUSB;
-                digitalWrite(USB, 1);
+        digitalWrite(USB, 1);
 		}
-		
-		  
-///	  SERIAL COMMUNICATION - remote computer control for DDS - worked but not finishet yet - 1, 2, 3, 4 - worked 
-   /*  check if data has been sent from the computer: */
-  if (Serial.available()) {
-    /* read the most recent byte */
-    byteRead = Serial.read();
-	if(byteRead == 49){     // 1 - up freq
-		rx = rx + increment;
-    Serial.println(rx);
-		}
-	if(byteRead == 50){		// 2 - down freq
-		rx = rx - increment;
-    Serial.println(rx);
-		}
-	if(byteRead == 51){		// 3 - up increment
-		setincrement();
-    Serial.println(increment);
-		}
-	if(byteRead == 52){		// 4 - print VFO state in serial console
-		Serial.println("VFO_VERSION 8.0");
-		Serial.println(rx);
-		Serial.println(rxif);
-		Serial.println(increment);
-		Serial.println(hertz);
-		}
-        if(byteRead == 53){		// 5 - scan freq from 7000 to 7050 and back to 7000
-             for (int i=0; i=500; (i=i+100))
-                rx = rx + i;
-                sendFrequency(rx);
-                Serial.println(rx);
-                showFreq();
-                display.clearDisplay();	
-				display.setCursor(0,0);
-				display.println(rx);
-				display.setCursor(0,18);
-				display.println(hertz);
-				display.display();
-                delay(250);
-                }
-	}
+
 }	  
 /// END of main loop ///
 /// ===================================================== END ============================================
@@ -370,22 +281,19 @@ ISR(PCINT2_vect) {
   if (result) {    
     if (result == DIR_CW){rx=rx+increment;}
     else {rx=rx-increment;};       
-      if (rx >=160000000){rx=rx2;}; // UPPER VFO LIMIT 
+      if (rx >=130000000){rx=rx2;}; // UPPER VFO LIMIT 
       if (rx <=100000){rx=rx2;}; // LOWER VFO LIMIT
   }
 }
 
 // frequency calc from datasheet page 8 = <sys clock> * <frequency tuning word>/2^32
 void sendFrequency(double frequency) {  
-  int32_t freq = (frequency + rxif) * 4294967296./180000000;  // note 180 MHz clock on 9851. also note slight adjustment of this can be made to correct for frequency error of onboard crystal
+  int32_t freq = ((frequency + rxif) - rit) * 4294967296./180000000;  // note 180 MHz clock on 9851. also note slight adjustment of this can be made to correct for frequency error of onboard crystal
   for (int b=0; b<4; b++, freq>>=8) {
     tfr_byte(freq & 0xFF);
   }
   tfr_byte(0x001);   // Final control byte, LSB 1 to enable 6 x xtal multiplier on 9851 set to 0x000 for 9850
   pulseHigh(FQ_UD);  // Done!  Should see output
-  
-//    Serial.println(frequency);   // for serial console debuging
-//    Serial.println(frequency + rxif);
 }
 
 // transfers a byte, a bit at a time, LSB first to the 9851 via serial DATA line
@@ -409,18 +317,9 @@ void setincrement(){
   else if (increment == 10000){increment = 100000; hertz="100 Khz"; hertzPosition=0;}
   else if (increment == 100000){increment = 1000000; hertz="1 Mhz"; hertzPosition=0;} 
   else{increment = 1; hertz = "1 Hz"; hertzPosition=0;};  
-//   lcd.setCursor(0,1);
-//   lcd.print("           ");
-//   lcd.setCursor(hertzPosition,1); 
-//   lcd.print(hertz);
-  display.clearDisplay();	
-  display.setCursor(0,0);
-  display.println(rx);
-  display.setCursor(0,18);
-  display.println(hertz);
-  display.display();
-  delay(250); // Adjust this delay to speed up/slow down the button menu scroll speed.
-};
+	showFreq();
+    delay(250); // Adjust this delay to speed up/slow down the button menu scroll speed.
+}
 
 // oled display functions
 void showFreq(){
@@ -440,21 +339,7 @@ void showFreq(){
 	display.display();
 
 	timepassed = millis();
-    memstatus = 0; // Trigger memory write
-};
-
-void storeMEM(){
-   //Write each frequency section to a EPROM slot.  Yes, it's cheating but it works!
-   EEPROM.write(0,millions);
-   EEPROM.write(1,hundredthousands);
-   EEPROM.write(2,tenthousands);
-   EEPROM.write(3,thousands);
-   EEPROM.write(4,hundreds);       
-   EEPROM.write(5,tens);
-   EEPROM.write(6,ones);   
-   memstatus = 1;  // Let program know memory has been written
-};
-
+}
 
 void checkBTNdecode(){
 
@@ -506,12 +391,11 @@ switch (BTNinc) {
       }
     break;
   }
-//  lcd.clear(); 	//for lcd only - CHECK IF NEED uncoment 
+ 
 }
 
 if(BTNdecodeON == LOW){
     BTNcheck2 = 0;
-//  lcd.clear();   //for lcd only - CHECK IF NEED uncoment 
 	}
     BTNlaststate = BTNcheck2;
   }
